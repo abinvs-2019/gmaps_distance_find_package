dbb18d7e0d6940dfe4bef9d090afd47ee774447b
abin <abin.tichtech@gmail.com>
2022-06-02 13:09:12 +0530

    [INITIAL COMMIT]

---
+++ lib/gmaps_by_road_distance_calculator.dart
@@ -0,0 +1,67 @@
+library gmaps_by_road_distance_calculator;
+
+import 'dart:math';
+
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GetDistanceWithPolyLines {
+  // Object for PolylinePoints
+  late PolylinePoints polylinePoints;
+
+// List of coordinates to join
+  List<LatLng> polylineCoordinates = [];
+
+// Create the polylines for showing the route between two places
+
+  createPolylines(gmapsApiKey,
+      {required double startLatitude,
+      required double startLongitude,
+      required double destinationLatitude,
+      required double destinationLongitude}) async {
+    // Initializing PolylinePoints
+    polylinePoints = PolylinePoints();
+    // Generating the list of coordinates to be used for
+    // drawing the polylines
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      '', // Google Maps API Key
+      PointLatLng(startLatitude, startLongitude),
+      PointLatLng(destinationLatitude, destinationLongitude),
+      travelMode: TravelMode.transit,
+    );
+    // Adding the coordinates to the list
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    loopIt();
+  }
+
+  double _coordinateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
+
+  double totalDistance = 0.0;
+
+// Calculating the total distance by adding the distance
+// between small segments
+
+  var _placeDistance;
+  loopIt() {
+    for (int i = 0; i < polylineCoordinates.length - 1; i++) {
+      totalDistance += _coordinateDistance(
+        polylineCoordinates[i].latitude,
+        polylineCoordinates[i].longitude,
+        polylineCoordinates[i + 1].latitude,
+        polylineCoordinates[i + 1].longitude,
+      );
+    }
+    _placeDistance = totalDistance.toStringAsFixed(2);
+  }
+}
---
+++ test/gmaps_by_road_distance_calculator_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gmaps_by_road_distance_calculator/gmaps_by_road_distance_calculator.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = GetDistanceWithPolyLines();
+  });
+}
