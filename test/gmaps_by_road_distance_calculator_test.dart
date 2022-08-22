9631e8397b84850ed47aa62b0742d11b0104f1b5
abin <abin.tichtech@gmail.com>
2022-08-22 16:29:39 +0530

    Release v1.0.1

--- test/gmaps_by_road_distance_calculator_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gmaps_by_road_distance_calculator/gmaps_by_road_distance_calculator.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final calculator = GetDistanceWithPolyLines();
-  });
-}
