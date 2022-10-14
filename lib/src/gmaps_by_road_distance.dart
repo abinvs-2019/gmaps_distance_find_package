2b9f072505f28d2217636374977e852d441d163a
abin <abinvs2019@gmail.com>
2022-10-14 12:37:18 +0530

    Clean up

--- lib/src/gmaps_by_road_distance.dart
+++ lib/src/gmaps_by_road_distance.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter_polyline_points/flutter_polyline_points.dart' as poly;
 import 'package:google_maps_flutter/google_maps_flutter.dart' as Gmap;
 import 'package:latlong2/latlong.dart';
@@ -23,6 +24,7 @@ class DistanceCalculator {
     polylinePoints = poly.PolylinePoints();
     // Generating the list of coordinates to be used for
     // drawing the polylines
+    print('calling method');
     poly.PolylineResult result =
         await polylinePoints.getRouteBetweenCoordinates(
             '$gmapsApiKey', // Google Maps API Key
@@ -35,23 +37,31 @@ class DistanceCalculator {
                     : travelMode == TravelModes.walking
                         ? poly.TravelMode.walking
                         : poly.TravelMode.transit);
+    print(result.points);
+    print(result.errorMessage);
+    print(result.status);
     // Adding the coordinates to the list
     if (result.points.isNotEmpty) {
       for (var point in result.points) {
         polylineCoordinates.add(Gmap.LatLng(point.latitude, point.longitude));
       }
+    } else {
+      print('Polylines are emplty');
     }
-    print(result.points);
-    print(result.errorMessage);
-    print(result.status);
-    return await loopIt();
+    return loopIt();
   }
 
   double totalDistance = 0.0;
 
+  getDistanceFromPointsToPoints(LatLng start, LatLng end) {
+    final num km = distance.as(LengthUnit.Kilometer, start, end);
+    print(km);
+    return km;
+  }
+
 // Calculating the total distance by adding the distance
 // between small segments
-  loopIt() async {
+  loopIt() {
     for (int i = 0; i < polylineCoordinates.length - 1; i++) {
       totalDistance += distance(
           LatLng(polylineCoordinates[i].latitude,
@@ -59,8 +69,7 @@ class DistanceCalculator {
           LatLng(polylineCoordinates[i + 1].latitude,
               polylineCoordinates[i + 1].longitude));
     }
-
-    totalDistance = totalDistance / 1000;
+    totalDistance = totalDistance / 1000; // to km
     return totalDistance
         .toStringAsFixed(2); // Would return 0.0 km on any exception
   }
